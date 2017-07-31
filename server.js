a57c1f8a9be107a62bb685858532205a843808d7
Haversham94 <rodolpheaguidissou@gmail.com>
2017-07-31 11:01:57 +0200

    add boilerplate backend && mocked datasource

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // Main file for the server
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const http = require('http');
 const app = express();
 
+const router = require('./router');
+
 // App setup
+app.use(morgan('combined')); // for logging and debugging
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+server.listen(PORT);
+console.log('Server is listening on port: ', PORT);
