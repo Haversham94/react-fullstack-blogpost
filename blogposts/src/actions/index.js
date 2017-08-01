df75e50d6ffc232a74e5a6877f405d649ecc9bb2
Haversham94 <rodolpheaguidissou@gmail.com>
2017-08-01 14:27:01 +0200

    added more action creators and their handles

--- blogposts/src/actions/index.js
+++ blogposts/src/actions/index.js
@@ -1,12 +1,39 @@
 import axios from 'axios';
 
-import { BACKEND_URL } from '../constants';
-import { FETCH_POSTS } from './types';
+import { FETCH_POSTS, CREATE_POSTS, FETCH_POST, DELETE_POST } from './types';
 
 export const fetchPosts = () => {
-    const request = axios.get(`${BACKEND_URL}/`);
+    const request = axios.get('/api/posts');
     return {
         type: FETCH_POSTS,
         payload: request
+    };
+};
+
+export const createPosts = (values, cb) => {
+    const request = axios.post('/api/posts', values).then(() => {
+        cb();
+    });
+    return {
+        type: CREATE_POSTS,
+        payload: request
+    };
+};
+
+export const fetchPost = postId => {
+    const request = axios.get(`/api/posts/${postId}`);
+    return {
+        type: FETCH_POST,
+        payload: request
+    };
+};
+
+export const deletePost = (postId, cb) => {
+    axios.delete(`/api/posts/${postId}`).then(() => {
+        cb();
+    });
+    return {
+        type: DELETE_POST,
+        payload: postId
     };
 };
--- blogposts/src/reducers/postsReducer.js
+++ blogposts/src/reducers/postsReducer.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS } from '../actions/types';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/types';
 
 const initialState = {};
 export default function(state = initialState, action) {
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST: {
+            const post = action.payload.data;
+            return {
+                ...state,
+                [post.id]: post
+            };
+        }
         default:
             return state;
     }
