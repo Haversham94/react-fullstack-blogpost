dc0d468063e2f28e2bdb43b378cf0e4b5711aa12
Haversham94 <rodolpheaguidissou@gmail.com>
2017-08-02 09:56:59 +0200

    added Signin route

--- blogposts/src/index.js
+++ blogposts/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import promise from 'redux-promise';
+import reduxThunk from 'redux-thunk';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -13,13 +14,14 @@ import PostsList from './components/PostsList.jsx';
 import PostsDetail from './components/PostsDetail.jsx';
 import PostsNew from './components/PostsNew.jsx';
 import Header from './components/Header.jsx';
+import Signin from './components/auth/Signin.jsx';
 
 //store setup
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
     {},
-    composeEnhancers(applyMiddleware(promise))
+    composeEnhancers(applyMiddleware(promise, reduxThunk))
 );
 
 ReactDOM.render(
@@ -28,6 +30,7 @@ ReactDOM.render(
             <div>
                 <Header />
                 <Switch>
+                    <Route path="/signin" component={Signin} />
                     <Route path="/posts/new" component={PostsNew} />
                     <Route path="/posts/:id" component={PostsDetail} />
                     <Route path="/" component={PostsList} />
