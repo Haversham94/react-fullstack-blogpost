d0f353497af0d30b49258c8c1b07c63d8365ce1a
Rodolphe <rodoplhe.aguidissou@experian.com>
2017-08-02 15:00:59 +0200

    added using of react-redux-router

--- blogposts/src/reducers/index.js
+++ blogposts/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { routerReducer } from 'react-router-redux';
+
 import postsReducer from './postsReducer';
 import authReducer from './authReducer';
 
 export default combineReducers({
     posts: postsReducer,
     form: formReducer,
-    auth: authReducer
+    auth: authReducer,
+    router: routerReducer
 });
