57cd8156b9cacd491f59678bb7afd2dc80d58bd9
Haversham94 <rodolpheaguidissou@gmail.com>
2017-08-02 09:57:35 +0200

    improve header component

--- blogposts/src/components/Header.jsx
+++ blogposts/src/components/Header.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = () => {
-    return (
-        <nav className="navbar navbar-inverse">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <span className="navbar-brand">ARES</span>
+class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button
+                            type="button"
+                            className="navbar-toggle collapsed"
+                            data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1"
+                            aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                        </button>
+                        <span className="navbar-brand">ARES</span>
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li className="active nav-item">Sign in</li>
+                        <li className="active nav-item">Sign in</li>
+                    </ul>
                 </div>
-            </div>
-        </nav>
-    );
-};
+            </nav>
+        );
+    }
+}
 
 export default Header;
