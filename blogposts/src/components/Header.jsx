76913ef307cdbe465257ac353f4b4fe9002370b2
Rodolphe <rodoplhe.aguidissou@experian.com>
2017-08-02 12:10:00 +0200

    improve header component using bootstrap and conditional rendering

--- blogposts/src/components/Header.jsx
+++ blogposts/src/components/Header.jsx
@@ -1,32 +1,62 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderLink() {
+        const { auth } = this.props;
+        if (auth) {
+            return (
+                <li>
+                    <Link to="/signout" className="nav-link">
+                        Sign Out
+                    </Link>
+                </li>
+            );
+        } else {
+            return [
+                <li key={1}>
+                    <Link to="/signin" className="nav-link">
+                        Sign In
+                    </Link>
+                </li>,
+                <li key={2}>
+                    <Link to="/signup" className="nav-link">
+                        Sign Up
+                    </Link>
+                </li>
+            ];
+        }
+    }
     render() {
         return (
             <nav className="navbar navbar-inverse">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button
-                            type="button"
-                            className="navbar-toggle collapsed"
-                            data-toggle="collapse"
-                            data-target="#bs-example-navbar-collapse-1"
-                            aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                        </button>
-                        <span className="navbar-brand">ARES</span>
+                        <Link to="/" className="navbar-brand btn btn-link">
+                            @RES TECH
+                        </Link>
                     </div>
                     <ul className="nav navbar-nav">
-                        <li className="active nav-item">Sign in</li>
-                        <li className="active nav-item">Sign in</li>
+                        <li className="active">
+                            <a className="btn btn-link">Link</a>
+                        </li>
+                        <li>
+                            <a className="btn btn-link">Link</a>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right">
+                        {this.renderLink()}
                     </ul>
                 </div>
             </nav>
         );
     }
 }
+function mapStateToProps(state) {
+    return {
+        auth: state.auth.authenticated
+    };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
