f1d00e371efb6ada26cef0d68b3c544b25e4cdf2
Haversham94 <rodolpheaguidissou@gmail.com>
2017-08-02 09:56:18 +0200

    added signin component and authentication reducer

---
+++ blogposts/src/components/auth/Signin.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { signinUser } from '../../actions';
+
+class Signin extends Component {
+    constructor() {
+        super();
+
+        this.renderField = this.renderField.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>
+                    {field.label}
+                </label>
+                <input
+                    {...field.input}
+                    type={field.type}
+                    className="form-control"
+                />
+            </div>
+        );
+    }
+    onFormSubmit(values) {
+        this.props.signinUser(values, () => {
+            this.props.history.push('/');
+        });
+    }
+    render() {
+        const { handleSubmit } = this.props;
+        const { renderField, onFormSubmit } = this;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <Field
+                            label="Email"
+                            type="text"
+                            name="email"
+                            component={renderField}
+                        />
+                        <Field
+                            label="Password"
+                            type="password"
+                            name="password"
+                            component={renderField}
+                        />
+                        <button
+                            onClick={handleSubmit(onFormSubmit)}
+                            type="submit"
+                            className="btn btn-primary">
+                            Sign in
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'signin'
+})(connect(null, { signinUser })(Signin));
