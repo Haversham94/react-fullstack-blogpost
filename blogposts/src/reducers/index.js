18a77640521ea0c6b095eb46acc6a9ea24cd675b
Rodolphe <rodoplhe.aguidissou@experian.com>
2017-08-02 12:13:18 +0200

    added auth piece of state and handle auth_error action type

--- blogposts/src/reducers/index.js
+++ blogposts/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import postsReducer from './postsReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
     posts: postsReducer,
-    form: formReducer
+    form: formReducer,
+    auth: authReducer
 });
